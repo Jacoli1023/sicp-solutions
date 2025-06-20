8894379bf83a5d6adc94529632ab2e504140603c
Jacob Oliver <jacoli1023@gmail.com>
2025-06-20 14:28:34 -0500

    Added 'next' procedure from exercise 1.23 and refactored 'prime?' procedure to include nested definitions.

--- src/chapter-1/1-2/divisors.rkt
+++ src/chapter-1/1-2/divisors.rkt
@@ -3,12 +3,13 @@
 
 (define (square x) (* x x))
 
-(define (smallest-divisor n) (find-divisor n 2))
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
-(define (divides? a b) (= (remainder b a) 0))
-
 (define (prime? n)
+  (define (divides? a b) (= (remainder b a) 0))
+  (define (next n)
+    (if (= n 2) 3 (+ n 2)))
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (next test-divisor)))))
+  (define (smallest-divisor n) (find-divisor n 2))
   (= n (smallest-divisor n)))
