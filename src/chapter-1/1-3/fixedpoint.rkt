2d38aaf060083a3e09a5310e1dcf04dc349433ff
Jacob Oliver <jacoli1023@gmail.com>
2025-07-01 16:04:13 -0500

    Readded fixed-point-verbose to fixedpoint.rkt

--- src/chapter-1/1-3/fixedpoint.rkt
+++ src/chapter-1/1-3/fixedpoint.rkt
@@ -6,6 +6,16 @@
 (define (close-enough? x y)
   (< (abs (- x y)) tolerance))
 
+(define (fixed-point-verbose f first-guess)
+  (define (try guess)
+    (let ((next (f guess)))
+      (display next)
+      (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
 (define (fixed-point f first-guess)
   ((iterative-improve
     (lambda (guess)
