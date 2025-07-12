54c91cad46c099a816c92d5590ffa056b295c956
Jacob Oliver <jacoli1023@gmail.com>
2025-07-12 14:48:43 -0500

    Added exercises 2.24. Testing out diagram markdown syntax.

---
+++ src/chapter-2/2-2/tree-operations.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(provide (all-defined-out))
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
